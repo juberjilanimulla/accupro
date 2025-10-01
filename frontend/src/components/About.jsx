9ec2b722f5f826a1be6db159b44d21e05c54e498
Juber_Mulla <juberjilanimulla@gmail.com>
2025-10-01 16:14:35 +0530

    this is commit for changes in css

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/About.css";
+import { FiSearch } from "react-icons/fi";
 
 const About = () => {
   return (
@@ -13,15 +14,21 @@ const About = () => {
           </h2>
           <div className="about-stats">
             <div className="about-stats-box1">
-              <h3>86M+</h3>
+              <h1>
+                86<span>M+</span>
+              </h1>
               <p>Available Jobs</p>
             </div>
             <div className="about-stats-box2">
-              <h3>546+</h3>
+              <h1>
+                546<span>+</span>
+              </h1>
               <p>Free Courses</p>
             </div>
             <div className="about-stats-box3">
-              <h3>5.7M+</h3>
+              <h1>
+                5.7<span>M+</span>
+              </h1>
               <p>Alumni Has Worked</p>
             </div>
           </div>
@@ -36,11 +43,18 @@ const About = () => {
             Empowering individuals to find workplaces that value diversity and
             uniquesness.
           </p>
-          <span className="about-join">100k+ People Join</span>
-          <div className="about-people">
-            <img src="/about-img.png" alt="About section" />
+          <p className="about-join">
+            <span>+100k</span> People Join
+          </p>
+          <div className="about-img-wrapper">
+            <div className="about-people">
+              <img src="/assets/hero/hero-main3.png" alt="" />
+            </div>
+            <button className="about-button">
+              <span>Tracer Study</span>
+              <FiSearch className="search-icon" />
+            </button>
           </div>
-          <button className="about-button">Tracer Study</button>
         </div>
       </div>
     </section>
