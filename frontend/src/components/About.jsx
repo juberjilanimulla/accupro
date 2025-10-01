c16395abec15484ec674b65c7aa04753f8849406
Juber_Mulla <juberjilanimulla@gmail.com>
2025-10-01 10:58:15 +0530

    this is first commit about section

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -5,29 +5,28 @@ const About = () => {
   return (
     <section className="about">
       <div className="about-container">
-        <div className="about-content">
-          <h2>
+        {/* Left Side (Heading + Description + Stats) */}
+        <div className="about-left">
+          <h3 className="about-tagline">About us</h3>
+          <h2 className="about-title">
             Inclusive Opportunities, <br /> Tailored for You.
           </h2>
-          <p>
-            Jobs that embrace your uniqueness. Empowering individuals who thrive
-            through diversity and differences.
-          </p>
           <div className="about-stats">
-            <div>
+            <div className="about-stats-box">
               <h3>86M+</h3>
               <p>Available Jobs</p>
             </div>
-            <div>
+            <div className="about-stats-box">
               <h3>546+</h3>
               <p>Free Courses</p>
             </div>
-            <div>
+            <div className="about-stats-box">
               <h3>5.7M+</h3>
-              <p>Resumes Uploaded</p>
+              <p>Alumni Has Worked</p>
             </div>
           </div>
         </div>
+
         <div className="about-image">
           <img src="/about-img.png" alt="About section" />
         </div>
